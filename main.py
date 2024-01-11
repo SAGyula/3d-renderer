df37119df5d9b89de152df112cdaba1b54b49514
sim05 <siadgyu@gmail.com>
2024-01-11 18:52:59 +0100

    rotation works, movement with rotation doesn't

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import math
-
 import pygame
+
+small_number: float = 0.001
+
+
+def degrees_to_radians(degs: float) -> float:
+    return degs * (math.pi / 180)
 
 
 class Face:
@@ -26,12 +31,15 @@ class Cuboid:
     cor1: pygame.Vector3
     cor2: pygame.Vector3
 
+    color: str
+
     faces: list[Face]
 
-    def __init__(self, cor1: tuple[float, float, float], cor2: tuple[float, float, float]) -> None:
+    def __init__(self, cor1: tuple[float, float, float], cor2: tuple[float, float, float], color: str) -> None:
         self.cor1 = pygame.Vector3(cor1)
         self.cor2 = pygame.Vector3(cor2)
         self.faces = []
+        self.color = color
 
         self.create_faces()
 
@@ -66,19 +74,23 @@ class Camera:
 
     def rotate_point(self, point: pygame.Vector3) -> tuple[float, float, float]:
         x, y, z = point
-        cam_dist: float = math.sqrt((self.position.x - x)**2 + (self.position.z - z)**2)
-        rot_x = cam_dist * math.cos(x) * self.rotation.x
-        rot_z = cam_dist * math.sin(z) * self.rotation.x
-        return rot_x, y, rot_z
+        x -= self.position.x
+        y -= self.position.y
+        rotation = degrees_to_radians(self.rotation.x)
+
+        rot_x = (y * math.cos(rotation) - x * math.sin(rotation)) + self.position.x
+        rot_y = (x * math.cos(rotation) + y * math.sin(rotation)) + self.position.y
+
+        return rot_x, rot_y, z
 
     def render_cuboid(self, object_: Cuboid) -> None:
         for face in object_.faces:
-            self.render_face(face)
+            self.render_face(face, object_.color)
 
-    def render_face(self, face: Face):
+    def render_face(self, face: Face, color: str):
         points: list[tuple[float, float]] = [self.get_point(i) for i in face.vertices]
 
-        pygame.draw.polygon(self.screen, "darkgreen", points, width=0)
+        pygame.draw.polygon(self.screen, color, points, width=0)
 
     def get_point(self, point: pygame.Vector3) -> tuple[float, float]:
         x, y, z = self.rotate_point(point)
@@ -87,25 +99,29 @@ class Camera:
         y_dist = y - self.position.y
         z_dist = z - self.position.z
 
-        y_pos = self.res[1] / 2 - (y_dist * self.screen_distance) / x_dist
-        z_pos = self.res[0] / 2 - (z_dist * self.screen_distance) / x_dist
+        y_pos = self.res[1] / 2 - (y_dist * self.screen_distance) / (x_dist + small_number)
+        z_pos = self.res[0] / 2 - (z_dist * self.screen_distance) / (x_dist + small_number)
 
-        return z_pos, y_pos
+        return y_pos, z_pos
 
     def try_move(self, keys, dt):
+        rotation = degrees_to_radians(360 - self.rotation.x)
+
         if keys[pygame.K_w]:
-            self.position.x += 10 * dt
+            self.position.x += 10 * dt * math.sin(rotation)
+            self.position.y += 10 * dt * math.cos(rotation)
         if keys[pygame.K_s]:
-            self.position.x -= 10 * dt
+            self.position.x -= 10 * dt * math.sin(rotation)
+            self.position.y -= 10 * dt * math.cos(rotation)
         if keys[pygame.K_a]:
-            self.position.z += 10 * dt
+            self.position.y += 10 * dt
         if keys[pygame.K_d]:
-            self.position.z -= 10 * dt
+            self.position.y -= 10 * dt
 
         if keys[pygame.K_LEFT]:
-            self.rotation.x += 10 * dt
+            self.rotation.x -= 50 * dt
         if keys[pygame.K_RIGHT]:
-            self.rotation.x -= 10 * dt
+            self.rotation.x += 50 * dt
 
 
 def main() -> None:
@@ -115,9 +131,10 @@ def main() -> None:
 
     dt = 0
 
-    cube = Cuboid((1, 1, 1), (2, 2, 2))
-    cube2 = Cuboid((2, 1, 2), (4, 4, 4))
-    camera = Camera(pygame.Vector3(-2, 2, 0), 800, (1280, 720), screen)
+    cube = Cuboid((1, 1, 1), (3, 2, 2), "blue")
+    cube2 = Cuboid((1, 1, 1), (2, 3, 2), "green")
+    cube3 = Cuboid((1, 1, 1), (2, 2, 3), "yellow")
+    camera = Camera(pygame.Vector3(-2, 0, 1), 800, (1280, 720), screen)
 
     running = True
 
@@ -132,6 +149,7 @@ def main() -> None:
         camera.try_move(keys, dt)
 
         camera.render_cuboid(cube)
+        camera.render_cuboid(cube3)
         camera.render_cuboid(cube2)
 
         pygame.display.flip()
